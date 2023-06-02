06a569337594b1288d31d176fdd7c42d29493328
Bamlak Deju Abera <91570985+tejex@users.noreply.github.com>
2023-06-02 10:45:42 -0700

    Update AuthContext.js
    Code clean up, added some line breaks between different sections of the code file.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,17 @@
 import {useEffect, createContext, useState } from 'react';
 import {useNavigate} from "react-router-dom";
-
-
+//*************************************************************************//
 export const AuthContext = createContext({
   authenticated: false,
   login: () => {},
   logout: () => {},
 });
-
+//*************************************************************************//
 export const AuthContextProvider = ({children}) =>{
 
     const [authenticated, setAuthenticated] = useState(false);
-
     const navigate = useNavigate();
   
-
     useEffect(() => {
       const token = localStorage.getItem('token');
   
@@ -27,8 +24,7 @@ export const AuthContextProvider = ({children}) =>{
         setAuthenticated(false); 
       } 
     }, []); 
-
-
+//*************************************************************************//
     const login = (token, user) => {
         window.localStorage.setItem('token',token.token);
         window.localStorage.setItem('user',user);
@@ -36,7 +32,7 @@ export const AuthContextProvider = ({children}) =>{
         setAuthenticated(true);
         navigate("/main");
       };
-    
+   //*************************************************************************//
     const logout = () => {
         window.localStorage.removeItem('token');
         window.localStorage.removeItem('user');
@@ -44,7 +40,7 @@ export const AuthContextProvider = ({children}) =>{
         setAuthenticated(false);
         navigate("/");
       };
-          
+//*************************************************************************//
       return (
         <AuthContext.Provider value={{ authenticated, login, logout }}>
           {children}
